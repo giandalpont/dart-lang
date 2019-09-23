cca1104bad1be142586940040780af852da1d2d3
Gian Dal Pont <giandalpont@gmail.com>
2019-09-23 20:51:01 -0300

    trabalhando com list e map

---
+++ basic/lists.dart
@@ -0,0 +1,8 @@
+void main () {
+  List number = [ 1, 2, 3, 4, 5, 6, 'string', 1.23, true ];
+  print(number);
+  print(number[6]);
+
+  Map user = { 'name': 'João', 'agr': 18, 'height': 1.82 };
+  print(user['name']);
+}
