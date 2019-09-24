478e7b0765a8a0015ab3df1db4e5ae161d6722a1
Gian Dal Pont <giandalpont@gmail.com>
2019-09-23 21:01:57 -0300

    var and dynamic

--- basic/variaveis.dart
+++ basic/variaveis.dart
@@ -2,7 +2,25 @@ void main () {
   int idade = 23;
   double altura = 1.73;
   bool dev = true;
-  print(idade);
-  print(altura);
-  print(dev);
+  // print(idade);
+  // print(altura);
+  // print(dev);
+
+  var nome = 'gian';
+  var idade2 = 43;
+  var val = true;
+  var total = [ 'gian', 43, true ];
+  print(nome);
+  print(idade2);
+  print(val);
+  print(total);
+
+  dynamic nome1 = 'gian';
+  dynamic idade3 = 43;
+  dynamic val1 = true;
+  dynamic total1 = [ 'gian', 43, true ];
+  print(nome1);
+  print(idade3);
+  print(val1);
+  print(total1);
 }
